e3e988992db352619379cb60d6d94d1ac392ca62
Valkyrux <philadephos.granata@gmail.com>
2022-01-19 17:07:02 +0100

    computed

--- src/components/ContentContainer.vue
+++ src/components/ContentContainer.vue
@@ -13,7 +13,6 @@ export default {
   data() {
     return {
       items: null,
-      textHere: this.giveMeText,
     };
   },
   props: {
@@ -26,34 +25,37 @@ export default {
   },
   methods: {
     getMoviesFromApi(aTextToSearch) {
-      if(aTextToSearch != "" || aTextToSearch != "null"){
-      axios
-        .get("https://api.themoviedb.org/3/search/movie", {
-          params: {
-            api_key: "66aa3f3384fe1b5ecfefc1d11f432e71",
-            language: "en-US",
-            page: 1,
-            query: aTextToSearch,
-            include_adult: "false",
-          },
-        })
-        .then((response) => {
-          console.log(response);
-          this.items = response.data.results;
-        })
-        .catch((error) => {
-          console.log(error);
-          console.log(this.giveMeText);
-        });
-    }},
+        axios
+            .get("https://api.themoviedb.org/3/search/movie", {
+                params: {
+                    api_key: "66aa3f3384fe1b5ecfefc1d11f432e71",
+                    language: "en-US",
+                    page: 1,
+                    query: aTextToSearch,
+                    include_adult: "false",
+                }
+            })
+            .then((response) => {
+                this.items = response.data.results;
+                console.log("ciao");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+    
   }, 
     computed: {
       setResultFromSearch() {
-        this.getMoviesFromApi(this.giveMeText);
-        console.log(this.items);
-        return this.items;
+          if (this.giveMeText != "") {
+            this.getMoviesFromApi(this.giveMeText);
+            return true;
+        } else {
+            return false;
+            
+        }
       }
-    },
+    }
 };
 </script>
 
