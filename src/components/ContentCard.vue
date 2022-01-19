d1833bfd6eb16c803179a070f646c1b4e86494c4
Valkyrux <philadephos.granata@gmail.com>
2022-01-19 17:15:56 +0100

    milestone_1

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,6 +1,9 @@
 <template>
   <li>
-    {{ movieData.title }}
+    <h2>{{ movieData.title }}</h2> 
+    <h2>{{ movieData.original_title }}</h2>   
+    <h3>{{ movieData.original_language }}</h3> 
+    <h4>{{ movieData.vote_average }}</h4>  
   </li>
 </template>
 
