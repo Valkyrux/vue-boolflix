432fbc19017975a24de6ea88646dc1402441e00d
Valkyrux <philadephos.granata@gmail.com>
2022-01-20 17:20:56 +0100

    country-flag

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,19 +1,52 @@
 <template>
   <li>
-    <h2>{{ movieData.title }}</h2> 
-    <h2>{{ movieData.original_title }}</h2>   
-    <h3>{{ movieData.original_language }}</h3> 
-    <h4>{{ movieData.vote_average }}</h4>  
+    <img
+      :src="'https://image.tmdb.org/t/p/w342' + movieData.poster_path"
+      alt=""
+    />
+    <h2>{{ movieData.title }}</h2>
+    <h2>{{ movieData.original_title }}</h2>
+    <CountryFlag v-if="isThereTheIcon" :country="generateIcon" size="normal" />
+    <h3 v-else>{{ this.movieData.original_language }}</h3>
+    <h4>{{ movieData.vote_average }}</h4>
   </li>
 </template>
 
 <script>
+// vue-country-flag
+import CountryFlag from "vue-country-flag";
+
 export default {
   name: "ContentCard",
   props: {
     movieData: {
       type: Object,
       required: true,
+    },
+  },
+  components: {
+    CountryFlag,
+  },
+  computed: {
+    generateIcon() {
+      let flagIcon = this.movieData.original_language;
+      if (flagIcon === "en") {
+        flagIcon = "gb";
+      }
+      return flagIcon;
+    },
+    isThereTheIcon() {
+      const countyIdList = require("../../country.json").map(
+        (element) => element.code
+      );
+      if (
+        countyIdList.includes(this.movieData.original_language) ||
+        this.movieData.original_language == "en"
+      ) {
+        return true;
+      } else {
+        return false;
+      }
     },
   },
 };
--- src/components/ContentContainer.vue
+++ src/components/ContentContainer.vue
@@ -1,6 +1,10 @@
 <template>
   <ul v-if="setResultFromSearch">
-    <ContentCard :movieData="movie" v-for="(movie,index) in items" :key="index" />
+    <ContentCard
+      :movieData="movie"
+      v-for="(movie, index) in items"
+      :key="index"
+    />
   </ul>
 </template>
 
@@ -25,37 +29,35 @@ export default {
   },
   methods: {
     getMoviesFromApi(aTextToSearch) {
-        axios
-            .get("https://api.themoviedb.org/3/search/movie", {
-                params: {
-                    api_key: "66aa3f3384fe1b5ecfefc1d11f432e71",
-                    language: "en-US",
-                    page: 1,
-                    query: aTextToSearch,
-                    include_adult: "false",
-                }
-            })
-            .then((response) => {
-                this.items = response.data.results;
-                console.log("ciao");
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        }
-    
-  }, 
-    computed: {
-      setResultFromSearch() {
-          if (this.giveMeText != "") {
-            this.getMoviesFromApi(this.giveMeText);
-            return true;
-        } else {
-            return false;
-            
-        }
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "66aa3f3384fe1b5ecfefc1d11f432e71",
+            language: "it-IT",
+            page: 1,
+            query: aTextToSearch,
+            include_adult: "false",
+          },
+        })
+        .then((response) => {
+          this.items = response.data.results;
+          console.log(response.data.results);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  computed: {
+    setResultFromSearch() {
+      if (this.giveMeText != "") {
+        this.getMoviesFromApi(this.giveMeText);
+        return true;
+      } else {
+        return false;
       }
-    }
+    },
+  },
 };
 </script>
 
